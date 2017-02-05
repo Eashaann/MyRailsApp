6dc4ae392bed7653113917a57e226e7d07863956
Easha Ann <donaeasha25@gmail.com>
2017-02-05 19:27:48 +0400

    product price data type change

--- db/migrate/20170201180650_add_color_to_products.rb
+++ db/migrate/20170201180650_add_color_to_products.rb
@@ -1,6 +1,6 @@
 class AddColorToProducts < ActiveRecord::Migration[5.0]
   def change
     add_column :products, :colour, :string
-    add_column :products, :price, :number
+    add_column :products, :price, :integer
   end
 end
