0cfb236d112ff8d5f8133d3e478f0f7f1d82e53c
Easha Ann <donaeasha25@gmail.com>
2017-02-20 22:24:36 +0400

    5.1 Routes related changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   resources :products
-  get 'static_pages/about'
+  get 'about', to: 'static_pages#about'
 
-  get 'static_pages/contact'
+  get 'contact', to: 'static_pages#contact'
 
-  get 'static_pages/index'
+  get 'welcome' , to: 'static_pages#index'
   
-  root 'static_pages#landing_page'
+  get 'featured', to:'static_pages#landing_page'
+
+  root 'products#index'
+
+
 
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
