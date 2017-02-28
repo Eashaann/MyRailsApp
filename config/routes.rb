837977714730f018194a0ecd3ef89e42f75e004c
Easha Ann <donaeasha25@gmail.com>
2017-02-28 07:50:23 +0400

    5.4 Added Authentication module using Device gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users,:path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout',:sign_up => 'register'}
+  resources :users
   resources :products
   get 'about', to: 'static_pages#about'
 
@@ -13,6 +15,7 @@ Rails.application.routes.draw do
   post 'static_pages/thank_you'
 
   resources :orders, only: [:index, :show, :create, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
