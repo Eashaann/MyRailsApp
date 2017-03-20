894b1b36069c5b6842a62e092d7ec7b6716955fe
Easha Ann <donaeasha25@gmail.com>
2017-03-21 00:14:52 +0400

    6.3 Added functional tests for static pages and users controllers

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+describe UsersController, :type => :controller do
+  
+  before do
+     @user = User.create!(email:"asd@123.com", password:"123456")
+     @user2 = User.create!(email:"xyz@123.com", password:"1234567")
+  end
+
+  describe 'GET #show' do 
+    context 'User is logged in' do
+      before do
+        sign_in @user
+        request.env["HTTP_REFERER"] = "/login"
+      end
+
+      it 'loads correct user details' do
+         get :show, id: @user.id
+         expect(response).to be_success
+         expect(response).to have_http_status(200)
+         expect(assigns(:user)).to eq @user
+      end
+
+      it 'no access to other user pages' do
+        get :show, id: @user2.id
+        expect(response).to redirect_to('/login')
+      end
+
+      it 'redirect the user with a HTTP 302 status code' do
+        get :show, id: @user2.id
+        expect(response).to have_http_status(302)
+      end
+    end
+
+    context 'No user is logged in' do
+
+      it 'redirects to login' do
+        get :show, id: @user.id
+        expect(response).to redirect_to('/login')
+      end
+    end
+  end
+ 
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'devise'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -54,4 +55,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # For Devise <= 4.1.0
+  config.include Devise::Test::ControllerHelpers, :type => :controller
+  # Use the following instead if you are on Devise >= 4.1.1
+  # config.include Devise::TestHelpers, :type => :controller
 end
